d167759f700c3f02fa67aaa4ed20539be2102fef
Yuichi Murata <mrk21info+github@gmail.com>
2014-06-08 23:39:38 +0900

    Fix the vim role.

--- roles/vim/files/macvim-kaoriya_profile.sh
+++ roles/vim/files/macvim-kaoriya_profile.sh
@@ -1,1 +1,2 @@
-PATH=/Applications/MacVim.app/Contents/MacOS:${PATH}
+export VIM_PATH=/Applications/MacVim.app/Contents/MacOS
+export PATH=${VIM_PATH}:${PATH}
---
+++ roles/vim/files/vim_profile.sh
@@ -0,0 +1,2 @@
+export VIM_CPP_STDLIB='/usr/include/c++/4.2.1'
+export VIM_CPP_INCLUDE="/usr/include,${VIM_CPP_STDLIB},/usr/local/include"
