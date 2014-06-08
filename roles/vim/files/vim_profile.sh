721bf89b1c22da0f214a99405f705a02d727697b
Yuichi Murata <mrk21info+github@gmail.com>
2014-06-09 00:00:31 +0900

    Fix the vimrc.

--- roles/vim/files/vim_profile.sh
+++ roles/vim/files/vim_profile.sh
@@ -1,2 +1,5 @@
 export VIM_CPP_STDLIB='/usr/include/c++/4.2.1'
 export VIM_CPP_INCLUDE="/usr/include,${VIM_CPP_STDLIB},/usr/local/include"
+
+export VIM_CLANG_PATH='/usr/bin/clang++'
+export VIM_CLANG_LIB_PATH='/Library/Developer/CommandLineTools/usr/lib'
