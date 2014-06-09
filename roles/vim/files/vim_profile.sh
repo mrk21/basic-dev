2618619d96ff217f8b3da3995cf13686d8b26df6
Yuichi Murata <mrk21info+github@gmail.com>
2014-06-09 13:43:45 +0900

    Fix the cpp stdlib settings on the vimrc.

--- roles/vim/files/vim_profile.sh
+++ roles/vim/files/vim_profile.sh
@@ -1,5 +1,5 @@
-export VIM_CPP_STDLIB='/usr/include/c++/4.2.1'
-export VIM_CPP_INCLUDE="/usr/include,${VIM_CPP_STDLIB},/usr/local/include"
+export VIM_CPP_STDLIB='/usr/include/c++/4.2.1,/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/c++/v1'
+export VIM_CPP_INCLUDE="${VIM_CPP_STDLIB},/usr/include,/usr/local/include"
 
 export VIM_CLANG_PATH='/usr/bin/clang++'
 export VIM_CLANG_LIB_PATH='/Library/Developer/CommandLineTools/usr/lib'
